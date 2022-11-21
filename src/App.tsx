03c72ee3ce8b796205e4f98cd5671a4fa8317f80
temirhanb <temirhan023@gmaul.com>
2022-11-21 18:42:20 +0500

    - init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { PresentPage } from "./Components/PresentPage";
+
+import style from './css/index.module.css';
+
+function App() {
+  return (
+    <div className={style.container}>
+      <PresentPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/PresentPage/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+
+export const PresentPage: React.FC = () => {
+
+  return (
+    <div>hello world</div>
+  )
+}
